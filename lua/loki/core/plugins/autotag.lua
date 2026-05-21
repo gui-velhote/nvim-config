0a7fd96893062a465f00e864c8930ebf2a8541cc
Guilherme Velhote <gvelhote@usp.br>
2026-05-21 09:25:21 -0300

    Adicionada ultima versão que utilizo

--- lua/loki/core/plugins/autotag.lua
+++ lua/loki/core/plugins/autotag.lua
@@ -2,6 +2,6 @@ require('nvim-ts-autotag').setup({
   opts = {
     enable_close = true,
     enable_rename = true,
-    enable_close_on_slash = true
+    enable_close_on_slash = false
   }
 })
--- lua/loki/lazy.lua
+++ lua/loki/lazy.lua
@@ -266,5 +266,15 @@ if not vim.loop.fs_stat(lazypath) then
           --   require 'wildcat.build'.build()
           -- end
         },
+        {
+          "ravitemer/mcphub.nvim",
+          dependencies = {
+            "nvim-lua/plenary.nvim",
+          },
+          build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+          config = function()
+            require("mcphub").setup()
+          end
+        }
       })
 
